46b1a011e28407afc0ce56b8b916fb3957ada436
Orion Buske <buske@cs.toronto.edu>
2015-02-24 17:28:27 -0500

    Simplified cohort script

--- is_same_cohort.py
+++ is_same_cohort.py
@@ -7,8 +7,8 @@
 import sys
 import os
 import logging
-import csv
 import string
+import js
 
 from collections import defaultdict
 
@@ -17,42 +17,24 @@ def read_cohorts(filename):
     with open(filename) as ifp:
         for line in ifp:
             line = line.strip()
-            cohorts = set(line.strip().split())
-            for cohort in cohorts:
-                if cohort in cohort_lookup:
-                    cohort_lookup[cohort].update(cohorts)
-                else:
-                    cohort_lookup[cohort] = cohorts
+            # Two-step cohort merging
+            cohort = set(line.strip().split())
+
+            # Combine cohorts of all patients in cohort
+            for patient in cohort:
+                cohort.update(cohort_lookup.get(patient, set()))
+
+            # Overwrite cohort for any patients
+            for patient in cohort:
+                cohort_lookup[patient] = cohort
+
     return cohort_lookup
 
-def read_id_lookup(filename):
-    ids = {}  # id -> external
-    with open(filename) as ifp:
-        reader = csv.DictReader(ifp, delimiter='\t')
-        for row in reader:
-            patient_id = row['Patient ID'].strip()
-            external_id = row['External ID'].strip()
-            if external_id:
-                ids[patient_id] = external_id
-    return ids
+def is_same_cohort(p1, p2, lookup):
+    return p1 == p2 or (p1 in lookup and p2 in lookup and lookup[p1] == lookup[p2])
 
-def get_cohort(p):
-    if p.startswith('UDP') or p.startswith('DDN'):
-        return p
-    else:
-        cohort = p.split('_')[0].rstrip(string.ascii_lowercase)
-        if cohort == '455CA': return '455'
-        return cohort
-
-def is_same_cohort(c1, c2, cohort_lookup):
-    return c1 == c2 or (c1 in cohort_lookup and c2 in cohort_lookup[c1])
-
-def script(pheno_sim, cohort_file, id_file=None):
+def script(matching_file, cohort_file):
     cohort_lookup = read_cohorts(cohort_file)
-
-    ids = None
-    if id_file:
-        ids = read_id_lookup(id_file)
 
     seen_ids = set()
     matches_found = defaultdict(set)
@@ -68,14 +50,7 @@ def script(pheno_sim, cohort_file, id_file=None):
             tokens = line.split('\t')
             p1, p2 = tokens[:2]
 
-            if ids:
-                p1 = ids.get(p1, p1)
-                p2 = ids.get(p2, p2)
-
-
-            c1 = get_cohort(p1)
-            c2 = get_cohort(p2)
-            same_cohort = is_same_cohort(c1, c2, cohort_lookup)
+            same_cohort = is_same_cohort(p1, p2, cohort_lookup)
 
             seen_ids.add(p1)
             seen_ids.add(p2)
@@ -83,7 +58,8 @@ def script(pheno_sim, cohort_file, id_file=None):
                 matches_found[p1].add(p2)
                 matches_found[p2].add(p1)
 
-            print('{:d}'.format(same_cohort))
+            # Print boolean as binary
+            print(int(same_cohort))
 
     for id in sorted(seen_ids):
         if id in matches_found:
@@ -100,10 +76,9 @@ def parse_args(args):
     description = __doc__.strip()
 
     parser = ArgumentParser(description=description)
-    parser.add_argument("--id-file")
-    parser.add_argument("pheno_sim")
+    parser.add_argument("matching_file", metavar="patients.sim")
     parser.add_argument("cohort_file", metavar="cohorts.txt",
-                        help="One cohort per line, whitespace separated patients")
+                        help="One cohort per line, whitespace separated patient IDs")
 
     return parser.parse_args(args)
 
